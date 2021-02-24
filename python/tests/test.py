157bfd069a26fe7010005d342cb96d24fe5ab5e2
Nicolas Cornu <nicolac76@yahoo.fr>
2021-02-24 11:55:53 +0100

    Add SonataError

--- python/tests/test.py
+++ python/tests/test.py
@@ -4,7 +4,7 @@ import unittest
 
 import numpy as np
 
-from libsonata import (Selection)
+from libsonata import (Selection, SonataError)
 from libsonatareport import (SpikeReader, SpikePopulation,
                        SomaReportReader, SomaReportPopulation,
                        ElementReportReader, ElementReportPopulation,
