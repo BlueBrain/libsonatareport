8353cc8b253ac84fd5f280bb8e5ab6828f5c1b16
Nicolas Cornu <nicolac76@yahoo.fr>
2021-02-24 13:45:10 +0100

    Try to clean

--- python/libsonatareport/__init__.py
+++ python/libsonatareport/__init__.py
@@ -1,3 +1,6 @@
+# Used by libsonatareport interface
+from libsonata import (Selection, SonataError)
+
 from libsonatareport._libsonatareport import (ElementReportPopulation,
                                               ElementReportReader,
                                               SomaReportPopulation,
--- python/tests/test.py
+++ python/tests/test.py
@@ -4,7 +4,6 @@ import unittest
 
 import numpy as np
 
-from libsonata import (Selection, SonataError)
 from libsonatareport import (SpikeReader, SpikePopulation,
                        SomaReportReader, SomaReportPopulation,
                        ElementReportReader, ElementReportPopulation,
