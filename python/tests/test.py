ff3e857045c860b387b7bc68fb898794ad7ff2a9
Nicolas Cornu <nicolac76@yahoo.fr>
2021-02-24 13:50:34 +0100

    Use them in tests

--- python/tests/test.py
+++ python/tests/test.py
@@ -4,7 +4,7 @@ import unittest
 
 import numpy as np
 
-from libsonatareport import (SpikeReader, SpikePopulation,
+from libsonatareport import (Selection, SonataError, SpikeReader, SpikePopulation,
                        SomaReportReader, SomaReportPopulation,
                        ElementReportReader, ElementReportPopulation,
                        )
